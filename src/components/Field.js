d6061c4c68380e9261b54da5d87663f49c64d733
Hasan Mubarok <hasan.mubarok@ruangguru.com>
2022-01-10 19:04:11 +0700

    initial commit

---
+++ src/components/Field.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Fields extends React.Component {
+    render() {
+        return (
+            <div className="ui field">
+                <label>Name</label>
+                <input />
+            </div>
+        )
+    }
+}
+
+export default Fields;
